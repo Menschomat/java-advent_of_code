273d9b92ce17a20785242122914052825e66cbec
Falco Weihrauch <falco@weihrauch.me>
2024-11-19 01:07:58 +0100

    disabled tests

--- src/test/java/de/menscho/space/y2022/day2/Day2Test.java
+++ src/test/java/de/menscho/space/y2022/day2/Day2Test.java
@@ -3,6 +3,7 @@ package de.menscho.space.y2022.day2;
 import de.menscho.space.utils.InputFileSize;
 import de.menscho.space.y2022.day1.Day1;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,12 +23,14 @@ class Day2Test {
     }
 
     @Test
+    @Disabled("NOT IMPLEMENTED")
     void solutionPart2Small() {
         int smallOut = Day2.solutionPart2(InputFileSize.SMALL);
         Assertions.assertEquals(-1, smallOut);
     }
 
     @Test
+    @Disabled("NOT IMPLEMENTED")
     void solutionPart2Full() {
         int fullOut = Day2.solutionPart2(InputFileSize.FULL);
         Assertions.assertEquals(-1, fullOut);
